008692abfcae147c3eef89295f76a76810288b07
Abirastra <156641759+Abirastra@users.noreply.github.com>
2024-02-20 08:46:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gelerifoto</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="login.php">GELERIFOTO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collaps" data-bs-target="#navbarNav" aria-controles="navbarNav" aria-expanded="false" aria-label="togglenavigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav me-auto">
+                    <a href="login.php" class="nav-link">Home</a>
+                    <a href="login.php" class="nav-link">Album</a>
+                    <a href="login.php" class="nav-link">Foto</a>
+                    <a href="login.php" class="nav-link">Notifokasi</a>
+                </div>
+              
+                <a href="login.php" class="btn btn-outline-success m-1">login</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="login.php"><img src="logo.jpg" class="card-img-top" title="" style="height:12rem;"></a>
+                    <div class="cars-foter text-center">
+                        <a href="login.php">10 suka</a>
+                        <a href="login.php">3 komen</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<footer class="text-center border-top mt-3 bg-light fixed-bottom">
+    <p>&copy; UKK 2024 | ABI RASTRA </p>
+</footer>
+            <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+    
+    
+</body>
+</html>
